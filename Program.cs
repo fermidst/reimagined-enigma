513bfc347af48e8c5fa7674ac8cfea7c7c5b9476
Arthur Sultanov <as@boolqa.com>
2020-05-07 20:41:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppDM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
