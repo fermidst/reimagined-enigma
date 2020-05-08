4f73febac09552942c63b398d15376e0523c7523
Arthur Sultanov <as@boolqa.com>
2020-05-08 03:24:37 +0300

    base version

---
+++ Evaluation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ConsoleAppDM
+{
+    public static class Evaluation
+    {
+        private static string[] Symbols { get; } =
+        {
+            "x",
+            "y",
+            "z",
+            "!x",
+            "!y",
+            "!z"
+        };
+
+        public static void GetTruthTable(int columns, int rows, string[][] array)
+        {
+            for (var i = 0; i < columns; i++)
+            for (var j = 0; j < rows; j++)
+                if ((j + 1) % (int) Math.Pow(2, columns - i) <= (int) Math.Pow(2, columns - i) / 2 &&
+                    (j + 1) % (int) Math.Pow(2, columns - i) != 0)
+                    array[j][i] = "0";
+                else
+                    array[j][i] = "1";
+        }
+
+        public static string GetPDNF(string[] funcValues, string[][] truthTable, int columns)
+        {
+            var result = string.Empty;
+            for (var i = 0; i < funcValues.Length; i++)
+            for (var j = 0; funcValues[i].Equals("1") && j < columns; j++)
+            {
+                result += truthTable[i][j].Equals("0") ? Symbols[j + Symbols.Length / 2] : Symbols[j];
+                if (j == columns - 1) result += " | ";
+            }
+
+            return string.IsNullOrEmpty(result) ? result : result.Substring(0, result.Length - 3);
+        }
+
+        public static string GetPCNF(string[] funcValues, string[][] truthTable, int columns)
+        {
+            var result = string.Empty;
+            var k = 1;
+            for (var i = 0; i < funcValues.Length; i++)
+            {
+                for (var j = 0; funcValues[i].Equals("0") && j < columns; j++)
+                {
+                    if (k == 1)
+                    {
+                        result += "(";
+                        k = 0;
+                    }
+
+                    result += truthTable[i][j].Equals("1") ? Symbols[j + Symbols.Length / 2] : Symbols[j];
+                    if (j == columns - 1)
+                        result += ") & ";
+                    else
+                        result += " | ";
+                }
+
+                k = 1;
+            }
+
+            return string.IsNullOrEmpty(result) ? result : result.Substring(0, result.Length - 3);
+        }
+
+        public static void WriteArray(int columns, int rows, string[][] truthTable, string functionVector)
+        {
+            for (var i = 0; i < columns; i++) Console.Write(Symbols[i] + " ");
+
+            Console.WriteLine("f");
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < columns; j++) Console.Write(truthTable[i][j] + " ");
+
+                Console.WriteLine(functionVector[i]);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleAppDM
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var funcVector = Console.ReadLine();
+
+            var columns = (int) (Math.Log(funcVector.Length) / Math.Log(2));
+            var rows = (int) Math.Pow(2, columns);
+
+            var truthTable = new string[rows][];
+            for (var i = 0; i < rows; i++) truthTable[i] = new string[columns];
+
+            var functionValues = funcVector.Select(c => c.ToString()).ToArray();
+            Evaluation.GetTruthTable(columns, rows, truthTable);
+            Console.WriteLine($"PDNF: {Evaluation.GetPDNF(functionValues, truthTable, columns)}");
+            Console.WriteLine($"PCNF: {Evaluation.GetPCNF(functionValues, truthTable, columns)}{Environment.NewLine}");
+
+            const int numberInGroup = 3;
+            const int groupLastDigit = 5;
+            var n1 = Convert.ToString(numberInGroup + groupLastDigit, 2);
+            var n2 = Convert.ToString(256 - (numberInGroup + groupLastDigit), 2);
+            if (n1.Length < 8 || n2.Length < 8)
+            {
+                for (var i = 8 - n1.Length; i > 0; i--) n1 = "0" + n1;
+
+                for (var i = 8 - n2.Length; i > 0; i--) n2 = "0" + n2;
+            }
+
+            var n1FunctionValues = n1.Select(c => c.ToString()).ToArray();
+            var n2FunctionValues = n2.Select(c => c.ToString()).ToArray();
+
+            Console.WriteLine(
+                $"N1 ({numberInGroup}+{groupLastDigit}) {n1} PDNF: {Evaluation.GetPDNF(n1FunctionValues, truthTable, columns)}");
+            Console.WriteLine(
+                $"N1 ({numberInGroup}+{groupLastDigit}) {n1} PCNF: {Evaluation.GetPCNF(n1FunctionValues, truthTable, columns)}");
+            Console.WriteLine(
+                $"N2 (256-({numberInGroup}+{groupLastDigit})) {n2} PDNF: {Evaluation.GetPDNF(n2FunctionValues, truthTable, columns)}");
+            Console.WriteLine(
+                $"N2 (256-({numberInGroup}+{groupLastDigit})) {n2} PCNF: {Evaluation.GetPCNF(n2FunctionValues, truthTable, columns)}");
         }
     }
 }
